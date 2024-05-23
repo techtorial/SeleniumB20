b22fe750492ff97beb73c52737b1b1b6a89bd53a
techtorial <50930535+techtorial@users.noreply.github.com>
2024-05-23 18:56:19 -0500

    Latest

--- src/test/java/com/qa/Bank/pages/BankManagerLoginPage.java
+++ src/test/java/com/qa/Bank/pages/BankManagerLoginPage.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import utils.BrowserUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class BankManagerLoginPage {
 
@@ -23,6 +27,20 @@ public class BankManagerLoginPage {
     WebElement postCode;
     @FindBy(xpath = "//button[@type='submit']")
     WebElement submitAddCustomer;
+    @FindBy(xpath = "//button[@ng-class='btnClass2']")
+    WebElement openAccountButton;
+    @FindBy(css = "#userSelect")
+    WebElement userDropDown;
+    @FindBy(name = "currency")
+    WebElement currency;
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement processButton;
+    @FindBy(xpath = "//button[@ng-class='btnClass3']")
+    WebElement customersButton;
+    @FindBy(xpath = "//input[@placeholder='Search Customer']")
+    WebElement searchCustomer;
+    @FindBy(css = ".ng-binding")
+    List<WebElement> allInformation;
 
 
 
@@ -35,6 +53,28 @@ public class BankManagerLoginPage {
         Alert alert=driver.switchTo().alert();
         Assert.assertTrue(alert.getText().contains(expectedMessage));
         alert.accept();
+    }
+
+    public void openAccount(WebDriver driver,String customerName,String currency,String expectedMessage){
+        openAccountButton.click();
+        BrowserUtils.selectBy(this.userDropDown,customerName,"text");
+        BrowserUtils.selectBy(this.currency,currency,"value");
+        processButton.click();
+        Alert alert=driver.switchTo().alert();
+        Assert.assertTrue(alert.getText().contains(expectedMessage));
+        alert.accept();
+    }
+
+    public void customers(String customerName,String firstName,String lastName,String postCode,String account){
+        customersButton.click();
+        searchCustomer.sendKeys(customerName);
+        List<String> expectedInformation= Arrays.asList(firstName,lastName,postCode,account);
+
+        for(int i=0; i<allInformation.size()-1;i++){
+            Assert.assertEquals(BrowserUtils.getText(allInformation.get(i)),expectedInformation.get(i));
+        }
+        Assert.assertTrue(allInformation.getLast().isDisplayed()); //getLast() -->Will give you the last  Element
+
     }
 
 }
--- src/test/java/com/qa/Bank/tests/BankManagerTest.java
+++ src/test/java/com/qa/Bank/tests/BankManagerTest.java
@@ -28,4 +28,49 @@ public class BankManagerTest {
         "Customer added successfully");
     }
 
+    @Test
+    public void validateOpenAccountFunctionality(){
+        //SETUP AUTOMATION
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().to("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+
+        //Click BankManagerLogin Button from LoginPage
+        BankLoginPage bankLoginPage=new BankLoginPage(driver);
+        bankLoginPage.clickManagerLoginButton();
+
+        //Provide All details in AddCustomer Button from BankManager Page
+        BankManagerLoginPage bankManagerLoginPage=new BankManagerLoginPage(driver);
+        bankManagerLoginPage.addCustomer(driver,"Ahmet","Baldir","60143",
+                "Customer added successfully");
+        bankManagerLoginPage.openAccount(driver,"Ahmet Baldir","Dollar",
+                "Account created successfully");
+    }
+
+    @Test
+    public void validateCustomersFunctionality(){
+        //SETUP AUTOMATION
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().to("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+
+        //Click BankManagerLogin Button from LoginPage
+        BankLoginPage bankLoginPage=new BankLoginPage(driver);
+        bankLoginPage.clickManagerLoginButton();
+
+        //Provide All details in AddCustomer Button from BankManager Page
+        BankManagerLoginPage bankManagerLoginPage=new BankManagerLoginPage(driver);
+        bankManagerLoginPage.addCustomer(driver,"Ahmet","Baldir","60143",
+                "Customer added successfully");
+
+        //Open Account Functionality
+        bankManagerLoginPage.openAccount(driver,"Ahmet Baldir","Dollar",
+                "Account created successfully");
+
+        //Customers Functionality
+        bankManagerLoginPage.customers("Ahmet","Ahmet","Baldir",
+                "60143","true");
+    }
 }
